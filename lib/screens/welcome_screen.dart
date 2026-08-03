d99cd660cc6b04a7d6e74900b083f9925b610967
xinyue05-xinyue <xykoh05@gmail.com>
2026-08-04 03:24:25 +0800

    commit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,7 +5,6 @@ import 'login_screen.dart';
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
